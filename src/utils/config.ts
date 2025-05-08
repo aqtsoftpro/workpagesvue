43250cb67239542fbe84b7408d107dd586b43344
jamshed45 <jamshed76@gmail.com>
2025-05-08 21:40:40 +0500

    WorkingPages Build 8may25

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -2,9 +2,15 @@
 // export const adminDashboardUrl = 'http://127.0.0.1:8000/loginWithEmail/';
 // export const adminDashboardDomain = 'http://127.0.0.1:8000/';
 
-export const apiUrl = 'https://wpages.aqtdemos.com/api/';
-export const adminDashboardUrl = 'https://wpages.aqtdemos.com/loginWithEmail/';
-export const adminDashboardDomain = 'https://wpages.aqtdemos.com/';
+// Staing 
+// export const apiUrl = 'https://wpages.aqtdemos.com/api/';
+// export const adminDashboardUrl = 'https://wpages.aqtdemos.com/loginWithEmail/';
+// export const adminDashboardDomain = 'https://wpages.aqtdemos.com/';
+
+// Live
+export const apiUrl = 'https://console.workpages.ws/api/';
+export const adminDashboardUrl = 'https://console.workpages.ws/loginWithEmail/';
+export const adminDashboardDomain = 'https://console.workpages.ws/';
 
 // if (location.hostname === "localhost")
 //     alert("It's a local server!");
